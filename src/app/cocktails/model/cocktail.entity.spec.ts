c941ab866febd046b7e4c67c97581bdd6f0e8ab0
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-09-12 23:30:12 -0500

    feat: cocktail entity added

---
+++ src/app/cocktails/model/cocktail.entity.spec.ts
@@ -0,0 +1,7 @@
+import { CocktailEntity } from './cocktail.entity';
+
+describe('CocktailEntity', () => {
+  it('should create an instance', () => {
+    expect(new CocktailEntity()).toBeTruthy();
+  });
+});
